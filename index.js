a9c94418bd4e218441792a6f14490c895401e516
1512433 <45399293+1512433@users.noreply.github.com>
2018-11-28 21:05:59 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,34 +2,32 @@ document.addEventListener('DOMContentLoaded', function(){
   var vid = document.getElementById('cartoon');
   var cvs = document.getElementById('canvas1');
   var ctx = cvs.getContext('2d');
-  var back = document.createElement('canvas');
-  var backcontext = back.getContext('2d');
+//   var back = document.createElement('canvas');
+//   var backcontext = back.getContext('2d');
 
-  var cvs_w, cvs_h;
+  var cvs_w , cvs_h;
 
   vid.addEventListener('play', function(){
-      cvs_w =vid.clientWidth;
+      cvs_w = vid.clientWidth;
       cvs_h = vid.clientHeight;
-      cvs.width = cvs_w;
-      cvs.height = cvs_h;
-      back.width = cvs_w;
-      back.height = cvs_h;
+    //   cvs.width = cvs_w;
+    //   cvs.height = cvs_h;
+    //   back.width = cvs_w;
+    //   back.height = cvs_h;
       
       
-      draw(vid, ctx, backcontext, cvs_w, cvs_h);
+      draw(vid, ctx, cvs_w, cvs_h);
   },false);
 }, false);
 
-function draw(v, c, bc, cw, ch) {
+function draw(v, c, cw, ch) {
   if(v.paused || v.ended) return false;
-  // First, draw it into the backing canvas
-  bc.drawImage(v,0,0, cw,ch);
+  // First, draw it into the canvas
+  c.drawImage(v,650,0, cw, ch);
 
-  // Grab the pixel data from the backing canvas
-  var idata = bc.getImageData(0,0,cw,ch);
-  var data_x = bc.getImageData(0,0,cw,ch);
-  var data_y = bc.getImageData(0,0,cw,ch);
-
+  // Grab the pixel data from the canvas
+  var idata = c.getImageData(650,0,cw,ch);
+ 
   // Loop through the pixels, turning them grayscale
   for(var i = 0; i < idata.data.length; i+=4) {
       var r = idata.data[i];
@@ -42,30 +40,17 @@ function draw(v, c, bc, cw, ch) {
   }
   var kernel_x = [+1, 0, -1, +2, 0, -2, +1, 0, -1];
   var kernel_y = [+1, +2, +1, 0, 0, 0, -1, -2, -1];
+  var data = [];
+  convolution(data,idata.data,cw,kernel_x, kernel_y);  //Đạo hàm theo truc x
 
-  convolution(data_x.data,idata.data,cw,kernel_x);  //Đạo hàm theo truc x
-  convolution(data_y.data,idata.data,cw,kernel_y);  //Đạo hàm theo trục y
-  
-    for(var i = 0; i < data_x.data.length; i++) {
-        // if(i%4==3) continue;   
-        if(i%4==3) data_x.data[i] = 255;
-        else 
-            var g_x = data_x.data[i]*data_x.data[i];
-            var g_y = data_y.data[i]*data_y.data[i];
-            var g = Math.sqrt(g_x + g_y);
-            if(g>255) g = 255;
-
-            data_x.data[i] = g;
-
-        // data_x.data[i+1] = data_y.data[i+1];
-        // data_x.data[i+2] = (data_x.data[i]+data_x.data[i+1])/4;
-        // data_x.data[i+3] = 255;
-    }
-  // idata.data = data;
+  for (var i = 0; i < idata.data.length; i++) {
+            idata.data[i] = data[i];
+}
+//   idata.data = data;
   // Draw the pixels onto the visible canvas
-  c.putImageData(data_x,0,0);
+  c.putImageData(idata,650,0);
   // Start over!
-  setTimeout(draw,0,v,c,bc,cw,ch);
+  setTimeout(draw,0,v,c,cw,ch);
 }
 
 // Tính tích chập theo kernel đưa vào
@@ -75,75 +60,47 @@ function draw(v, c, bc, cw, ch) {
 // - widthstep dùng để điều chỉnh lên dòng hay xuống dòng
 // - kernel_step dùng để điều chỉnh lùi cột hay tiến cột 
 // - i_local là biến chỉ vị trí xung quanh i 
-function convolution(data, idata, w, kernel, opaque=true, threshold_high=200, threshold_low=50){
+function convolution(data, idata, w, kernel_x,kernel_y, opaque=true, threshold=0){
 
-  var kernel_size = Math.sqrt(kernel.length); 
-
-  for(var i = 0; i < data.length; i+=4) {
+  var kernel_size = Math.sqrt(kernel_x.length); 
+  var kernel_step = [-1, -1, -1,0,0,0,1,1,1];
+  for(var i = 0; i < idata.length; i++) {
     //   if(i%4==3) continue;
       var alphaFac = opaque ? 1 : 0;
 
-      var g = 0;
-      var g1 = 0;
-      var g2 = 0;
-      var g3 = 0;
-      for(var k = 0; k < kernel.length; k+=1) {    
+      var g_x = 0;
+      var g_y = 0;
 
-        var kernel_step = ((k%kernel_size)-1)*4;
-        var widthstep = w*(Math.trunc(k/kernel_size)-1)*4;
-        var i_local = i + widthstep + kernel_step;
+      for(var k = 0; k < kernel_x.length; k+=1) {    
+        var widthstep = w*kernel_step[k]*4;
+        var i_local = i + widthstep + kernel_step[k]*4;
 
-        if(kernel_step==0 && widthstep==0) continue;
+        // if(kernel_step[k]==0 && widthstep==0) continue;
 
-        if(( i_local >= 0) && (i_local < data.length)){
+        if(( i_local >= 0) && (i_local < idata.length)){
             var x = idata[i_local];
-            var y = kernel[k];
-            g += x*y;
-        }   
-        var i1_local = i+1 + widthstep + kernel_step;
-        if(( i1_local >= 0) && (i1_local < data.length)){
-            var x = idata[i1_local];
-            var y = kernel[k];
-            g1 += x*y;
-        }   
-        var i2_local = i+2 + widthstep + kernel_step;
-        if(( i2_local >= 0) && (i2_local < data.length)){
-            var x = idata[i2_local];
-            var y = kernel[k];
-            g2 += x*y;
-        }   
-        var i3_local = i+3 + widthstep + kernel_step;
-        if(( i3_local >= 0) && (i3_local < data.length)){
-            var x = idata[i1_local];
-            var y = kernel[k];
-            g3 += x*y;
+            var y_x = kernel_x[k];
+            var y_y = kernel_y[k];
+            g_x += x*y_x;
+            g_y += x*y_y;
         }   
       } 
-      
+    g_x = Math.abs(g_x);
+    g_y = Math.abs(g_y);
+    if(i%4==3) {
+        g_x += alphaFac * (255 - g_x);
+        g_y += alphaFac * (255 - g_y);
+    }
         
-        g = Math.abs(g);
-        g1 = Math.abs(g1);
-        g2 = Math.abs(g2);
-        g3 = Math.abs(g3);
-        g3 += alphaFac * (255 - g3);
+    var g = Math.sqrt(g_x*g_x + g_y*g_y);
+    if(g > 255) {
+        g = 255;
+    }
 
-    // if(g > threshold_high && g < threshold_low ) {
-    //     g = 0;
-    // }
-    // if(g1 > threshold_high && g1 < threshold_low ) {
-    //     g1 = 0;
-    // }
-    // if(g2 > threshold_high && g2 < threshold_low ) {
-    //     g2 = 0;
-    // }
-    // if(g3 > threshold_high && g3 < threshold_low ) {
-    //     g3 = 0;
-    // }
-   
-      data[i] = g;
-      data[i+1] = g1;
-      data[i+2] = g2;
-      data[i+3] = g3;
+    if(g < threshold) {
+        g = 0;
+    }
+    data[i] = g;
       
   }
 }
